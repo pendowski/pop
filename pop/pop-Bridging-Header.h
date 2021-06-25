67bbadd0bdde7309804c087df5a54153834c5992
Jarek Pendowski <jarek@pendowski.com>
2021-06-25 13:37:07 +0200

    Added bridging header

---
+++ pop/pop-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  pop-Bridging-Header.h
+//  pop
+//
+//  Created by Jarek Pendowski on 25/06/2021.
+//  Copyright © 2021 Facebook. All rights reserved.
+//
+
+#ifndef pop_Bridging_Header_h
+#define pop_Bridging_Header_h
+#import "POP.h"
+
+#endif /* pop_Bridging_Header_h */
