fe23ec38fbed394e1082f0a550967d85c7ae0fb7
Jarek Pendowski <jarek@pendowski.com>
2021-06-25 13:16:05 +0200

    Initial SPM support

---
+++ Package.swift
@@ -0,0 +1,26 @@
+// swift-tools-version:5.3
+
+import PackageDescription
+
+let package = Package(
+    name: "pop",
+    platforms: [.iOS(.v12)],
+    products: [.library(name: "pop", targets: ["pop"])],
+    targets: [
+        .target(
+            name: "pop",
+            path: "pop",
+            exclude: ["pop-ios-Info.plist",
+                "pop-osx-Info.plist",
+                "pop-tvos-Info.plist",
+                "POP.h",
+                "Swift"],
+            cSettings: [
+                .headerSearchPath("."),
+                .headerSearchPath("WebCore"),
+                .headerSearchPath("./..")
+            ]
+        )
+    ],
+    cxxLanguageStandard: .cxx11
+)
